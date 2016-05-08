2149c416f99820636fe5e0b890e4558735baa16e
James Jester <elmojester@gmail.com>
2016-05-08 15:50:59 -0500

    Service.

--- main.js
+++ main.js
@@ -12,14 +12,24 @@ app.config(['$routeProvider', function($routeProvider){
     })
 }])
 
-app.controller('MainContact',['$scope', '$location', '$log', function($scope, $location, $log){
+app.service('myInfo', function(){
+    
+})
+
+app.controller('MainContact',['$scope', '$location', '$log', 'myInfo', function($scope, $location, $log, myInfo){
     $scope.buttonOne = function() {
         $location.path('/contact');
         $log.debug('We are on contact view');
     }
+    
+        $scope.word = '';
+        $scope.$watch('word', function(old, newer) {
+            $log.info('Old:'+old);
+            $log.info('New:'+newer);
+        });
 }])
 
-app.controller('ViewContact', ['$scope', '$location', '$log', function($scope, $location, $log){
+app.controller('ViewContact', ['$scope', '$location', '$log', 'myInfo', function($scope, $location, $log, myInfo){
     $scope.returnHome = function(){
         $location.path('/');
         $log.debug('We are on home view');
