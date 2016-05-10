118549a0b826f436751c7ef79bb021ab576e94c3
James Jester <elmojester@gmail.com>
2016-05-09 23:40:24 -0500

    Bunch of nonsense!

--- main.js
+++ main.js
@@ -12,26 +12,68 @@ app.config(['$routeProvider', function($routeProvider){
     })
 }])
 
-app.service('myInfo', function(){
+app.service('myInfo', [function(){
+     this.contactList = [];
+    //  var contact = {name : 'James'};
     
-})
+     
+}]);
 
 app.controller('MainContact',['$scope', '$location', '$log', 'myInfo', function($scope, $location, $log, myInfo){
-    $scope.buttonOne = function() {
-        $location.path('/contact');
-        $log.debug('We are on contact view');
+   
+    $scope.contactList = myInfo.contactList;
+    var clearInput = function(){
+        $scope.name = '';
+        $scope.num = '';
+        $scope.email = '';
+    };
+    
+    var anotherContact = function(){
+        // contact.name = $scope.name;
+        // contact.num = $scope.num;
+        // contact.email = $scope.email;
+        
+        var contactInfo = {name: $scope.name, num: $scope.num, email: $scope.email};
+        console.log(contactInfo);
+        myInfo.contactList.push(contactInfo);
     }
     
-        $scope.word = '';
-        $scope.$watch('word', function(old, newer) {
-            $log.info('Old:'+old);
-            $log.info('New:'+newer);
-        });
+    $scope.submit = function(name, num, email){
+        anotherContact()
+        // console.log(name,num,email);
+        console.log(myInfo.contactList);
+        clearInput();
+    }
+    $scope.details = function(){
+        
+        $location.path('/contact/{{contact.name}}');
+    }
+  
+       
 }])
 
-app.controller('ViewContact', ['$scope', '$location', '$log', 'myInfo', function($scope, $location, $log, myInfo){
+app.controller('ViewContact', ['$scope', '$location', '$log', 'myInfo', '$routeParams', function($scope, $location, $log, myInfo, $routeParams){
     $scope.returnHome = function(){
         $location.path('/');
         $log.debug('We are on home view');
+    } 
+    var theName = $routeParams.id;
+    
+    // var contact = myInfo.contactList.name.theName;
+    var contactArray = myInfo.contactList;
+    // $scope.details = myInfo.contactList.anotherContact.contact;
+    var person;
+    for(var i = 0; i < contactArray.length; i++){
+        var singleContact = contactArray[i];
+        if(singleContact.name === theName){
+            console.log(singleContact);
+            // $scope.contactList = singleContact;
+            person = singleContact;
+            break;
+        }
     }
+    $scope.contact = person;
+     console.log(person);
+    // console.log(i);
+    // $scope.contactList = myInfo.contactList[i];
 }])
